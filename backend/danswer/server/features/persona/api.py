a3d615911c286eed363b860512e11cc053dc1af0
Yuhong Sun <yuhongsun96@gmail.com>
2023-12-06 02:06:09 -0800

    checkpoint

--- backend/danswer/server/features/persona/api.py
+++ backend/danswer/server/features/persona/api.py
@@ -7,7 +7,7 @@ from danswer.auth.users import current_admin_user
 from danswer.auth.users import current_user
 from danswer.db.chat import fetch_persona_by_id
 from danswer.db.chat import fetch_personas
-from danswer.db.chat import mark_persona_as_deleted
+#from danswer.db.chat import mark_persona_as_deleted
 from danswer.db.chat import upsert_persona
 from danswer.db.document_set import get_document_sets_by_ids
 from danswer.db.engine import get_session
